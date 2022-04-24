012522d6ea1022d5d02d845172788a561e26cff6
Gary Antier <gary.antier@rafi.ph>
2022-04-24 21:13:02 +0800

    feat: item header

--- src/js/fluent-navigation-view.js
+++ src/js/fluent-navigation-view.js
@@ -71,19 +71,61 @@ class FluentNavigationViewMenuItems extends HTMLElement {
         // Styling
         const style = document.createElement('style');
         style.textContent = `
-            :host {
-                display: flex;
-                flex-direction: column;
-                font-size: 15px;
-                overflow-y: auto;
-                padding-top: 4px;
-                row-gap: 4px;
-                user-select: none;
-                width: 100%;
-            }
+        :host {
+            display: flex;
+            flex-direction: column;
+            font-size: 15px;
+            overflow-y: auto;
+            padding-top: 4px;
+            row-gap: 4px;
+            user-select: none;
+            width: 100%;
+        }
         `;
 
         this.shadowRoot.append(style, template.content.cloneNode(true));
+    }
+}
+
+class FluentNavigationViewItemHeader extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+        
+        // Content
+        const content = document.createElement('span');
+        content.setAttribute('class', 'content');
+        content.textContent =  this.getAttribute('content');
+        
+        // Template/Slot
+        const template = document.createElement('template');
+        template.innerHTML = '<slot />';
+
+        // Styling
+        const style = document.createElement('style');
+        style.textContent = `
+        :host {
+            align-items: center;
+            box-sizing: border-box;
+            display: flex;
+            height: 36px;
+            padding-left: 12px;
+            user-select: none;
+            width: 100%;
+        }
+
+        :host .content {
+            color: #5d5d5d;
+            font-family: 'Segoe UI Variable', sans-serif;
+            font-size: 14px;
+            font-weight: 600;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+        `;
+
+        this.shadowRoot.append(style, content, template.content.cloneNode(true));
     }
 }
 
@@ -114,100 +156,104 @@ class FluentNavigationViewItem extends HTMLElement {
         // Styling
         const style = document.createElement('style');
         style.textContent = `
-            .ms-Icon {
-                font-family: 'Segoe Fluent Icons', sans-serif;
-                text-rendering: optimizeLegibility;
-            }
+        .ms-Icon {
+            font-family: 'Segoe Fluent Icons', sans-serif;
+            text-rendering: optimizeLegibility;
+        }
 
-            ${SegoeFluentIcon.buildCss()}
+        ${SegoeFluentIcon.buildCss()}
 
-            :host {
-                max-height: 36px;
-                overflow: hidden;
-                user-select: none;
-                width: 100%;
-            }
+        :host {
+            max-height: 36px;
+            overflow: hidden;
+            user-select: none;
+            width: 100%;
+        }
 
-            :host([expanded]) {
-                max-height: 100%;
-            }
+        :host([expanded]) {
+            max-height: 100%;
+        }
 
-            /* Button */
-            .button {
-                align-items: center;
-                background-color: transparent;
-                border-radius: 5px;
-                box-sizing: border-box;
-                color: #191919;
-                display: flex;
-                height: 36px;
-                margin: 0 4px;
-                padding: 0 3px;
-                position: relative;
-                width: calc(100% - 8px);
-            }
-            
-            .button:hover,
-            :host([active]) .button {
-                background-color: #eaeaea;
-            }
-            
-            :host([active]) .button:hover {
-                background-color: #ededed;
-            }
+        /* Button */
+        .button {
+            align-items: center;
+            background-color: transparent;
+            border-radius: 5px;
+            box-sizing: border-box;
+            color: #191919;
+            display: flex;
+            height: 36px;
+            margin: 0 4px;
+            padding: 0 3px;
+            position: relative;
+            width: calc(100% - 8px);
+        }
+        
+        .button:hover,
+        :host([active]) .button {
+            background-color: #eaeaea;
+        }
+        
+        :host([active]) .button:hover {
+            background-color: #ededed;
+        }
 
-            /* Indicator */
-            :host([active]) .button::before {
-                background-color: #1976d2;
-                border-radius: 2px;
-                content: '';
-                display: block;
-                height: 16px;
-                left: 0;
-                position: absolute;
-                top: 10px;
-                width: 3px;
-            }
+        .button:active {
+            color: #838383 !important;
+        }
 
-            .button span {
-                margin: 0 9px;
-            }
+        /* Indicator */
+        :host([active]) .button::before {
+            background-color: #1976d2;
+            border-radius: 2px;
+            content: '';
+            display: block;
+            height: 16px;
+            left: 0;
+            position: absolute;
+            top: 10px;
+            width: 3px;
+        }
 
-            /* Icon */
-            .icon {
-                line-height: 15px;
-                width: 15px;
-            }
+        .button span {
+            margin: 0 9px;
+        }
 
-            /* Content */
-            .content {
-                flex-grow: 1;
-                font-family: 'Segoe UI Variable', sans-serif;
-                font-size: 14px;
-                font-weight: 500;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-            }
-            
-            /* Chevron */
-            .chevron::before {
-                content: '\\E70D';
-                font-size: 12px;
-            }
+        /* Icon */
+        .icon {
+            line-height: 15px;
+            width: 15px;
+        }
 
-            :host([expanded]) .chevron::before {
-                content: '\\E70E';
-            }
-            
-            /* Sub menu items offset. */
-            :host([sub-item]) .button {
-                padding-left: 39px;
-            }
-            
-            :host([sub-item]) .button::before {
-                left: 27px;
-            }
+        /* Content */
+        .content {
+            flex-grow: 1;
+            font-family: 'Segoe UI Variable', sans-serif;
+            font-size: 14px;
+            font-weight: 500;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+        
+        /* Chevron */
+        .chevron::before {
+            content: '\\E70D';
+            font-size: 12px;
+        }
+
+        :host([expanded]) .chevron::before {
+            content: '\\E70E';
+        }
+        
+        /* Sub menu items offset. */
+        :host([sub-item]) .button {
+            padding-left: 39px;
+        }
+        
+        :host([sub-item]) .button::before {
+            left: 27px;
+        }
         `;
 
         this.navigationEvent = new CustomEvent('navigation', {});
@@ -247,4 +293,5 @@ class FluentNavigationViewItem extends HTMLElement {
 
 customElements.define('fluent-navigation-view', FluentNavigationView);
 customElements.define('fluent-navigation-view-menu-items', FluentNavigationViewMenuItems);
+customElements.define('fluent-navigation-view-item-header', FluentNavigationViewItemHeader);
 customElements.define('fluent-navigation-view-item', FluentNavigationViewItem);
